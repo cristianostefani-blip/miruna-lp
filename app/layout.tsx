8b06ceec11aa5ae465e3fc271e872f352b0e78af
Cristiano Stefani <cristiano.stefani@gmail.com>
2026-04-08 20:42:01 -0300

    Corrige o tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,21 +43,21 @@ export default function RootLayout({
         geistMono.variable
       )}>
         
-        {/* GOOGLE TAG MANAGER */}
+{/* GOOGLE TAG MANAGER */}
         <Script
-        id="google-tag-manager"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-        __html: `
-          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s),
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-P2KVBMLS');
-    `,
-  }}
-/>
-      //  {/* FIM DO TAGMANAGER */}
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s),
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-P2KVBMLS');
+            `,
+          }}
+        />
+        {/* FIM DO TAGMANAGER */}
 
         {children}
       </body>
