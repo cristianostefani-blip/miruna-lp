418e1f63df739111c1a3b9f755bdf8dc1be8d1b7
Cristiano Stefani <cristiano.stefani@gmail.com>
2026-04-08 10:45:49 -0300

    implementa tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,24 +43,21 @@ export default function RootLayout({
         geistMono.variable
       )}>
         
-        {/* GOOGLE ANALYTICS OTIMIZADO (Não afeta o Lighthouse!) */}
+        {/* GOOGLE TAG MANAGER */}
         <Script
-          strategy="afterInteractive"
-          src={`https://www.googletagmanager.com/gtag/js?id=G-RDP05FJ9S9`}
-        />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-RDP05FJ9S9');
-            `,
-          }}
-        />
-        {/* FIM DO ANALYTICS */}
+        id="google-tag-manager"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+        __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s),
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-P2KVBMLS');
+    `,
+  }}
+/>
+      //  {/* FIM DO TAGMANAGER */}
 
         {children}
       </body>
